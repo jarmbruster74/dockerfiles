99aa9233e6f54c2c843e22fe21dea89037242cf3
Nick Cronin <nick@cronin.id.au>
2018-12-03 15:37:47 +1100

    Add aws-terraform

---
+++ aws-terraform/init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+saml2aws configure --idp-provider=ADFS --mfa=Auto --url=https://adfs.intelematics.com/adfs/ls/idpinitiatedsignon.aspx --username=$USERNAME --skip-prompt
+saml2aws login --profile=default --password="$PASS" --role=arn:aws:iam::$ACCOUNT:role/ADFS-Administrator --skip-prompt
+
+terraform init \
+	-backend-config "bucket=$STATE_BUCKET" \
+	-backend-config "region=$STATE_BUCKET_REGION" \
+	-from-module /usr/src \
+	-get-plugins=false
+
+terraform $@
